2457506a9f7d650c10fd0543162255029c12a16c
ymyldz <113609896+ymyldz@users.noreply.github.com>
2023-04-28 12:31:23 +0300

    Initial Commit

---
+++ Yummie/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Yummie
+//
+//  Created by Yusuf Mert Yıldız on 28.04.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
